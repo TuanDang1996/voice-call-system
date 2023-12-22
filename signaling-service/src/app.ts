d43bb26391a364d1135bd94e4d9895b3b65d132b
ndvinh98 <kaiznguyen98@gmail.com>
2023-12-22 08:52:29 +0000

    feat: add recording service

--- signaling-service/src/app.ts
+++ signaling-service/src/app.ts
@@ -1,20 +1,29 @@
-import * as express from 'express';
-import {WebSocket} from './websocket/websocket';
+import * as express from "express";
+import { WebSocket } from "./websocket/websocket";
 import * as http from "http";
+import path = require("path");
 
-const mediaHost =  process.env.MEDIA_SERVER_HOST || 'localhost';
-const mediaPort =  process.env.MEDIA_SERVER_PORT || '8899';
-const mediaUri= "ws://" + mediaHost + ":" + mediaPort + "/kurento"
+const mediaHost = process.env.MEDIA_SERVER_HOST || "10.0.0.13";
+const mediaPort = process.env.MEDIA_SERVER_PORT || "8899";
+const mediaUri = "ws://" + mediaHost + ":" + mediaPort + "/kurento";
 
-const appHost =  process.env.APP_SERVER_HOST || 'localhost';
-const appPort =  process.env.APP_SERVER_PORT || '8444';
-const asUri= "http://" + appHost + ":" + appPort + "/"
+const appHost = process.env.APP_SERVER_HOST || "0.0.0.0";
+const appPort = process.env.APP_SERVER_PORT || "8444";
+const asUri = "http://" + appHost + ":" + appPort + "/";
 
-const app:express.Express = express();
-const server = http.createServer(app).listen(appPort, function() {
-    console.log('Kurento Tutorial started');
-    console.log(`Open app http://localhost:${appPort} with a WebRTC capable browser`);
+const app: express.Express = express();
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, "static")));
+// app.get("/recording", (req, res) => {
+//   res.sendFile(path.join(__dirname, "static", "index.html"));
+// });
+const server = http.createServer(app).listen(appPort, function () {
+  console.log("Kurento Tutorial started");
+  console.log(
+    `Open app http://localhost:${appPort} with a WebRTC capable browser`
+  );
 });
 
 //init websocket
-new WebSocket(mediaUri, server)
+new WebSocket(mediaUri, server);
--- signaling-service/src/websocket/actions/OnIcandidate.ts
+++ signaling-service/src/websocket/actions/OnIcandidate.ts
@@ -1,19 +1,18 @@
-import {UserRegistry} from "../../model/UserRegistry";
-import {CachedData} from "../../helper/CachedData";
-import * as Kurento from 'kurento-client';
-import {UserSession} from "../../model/UserSession";
+import { UserRegistry } from "../../model/UserRegistry";
+import { CachedData } from "../../helper/CachedData";
+import * as Kurento from "kurento-client";
+import { UserSession } from "../../model/UserSession";
 
 export function onIceCandidate(sessionId: string, _candidate: any, name: any) {
-    const candidate = Kurento.getComplexType('IceCandidate')(_candidate);
-    const user:UserSession = UserRegistry.getById(sessionId);
+  const candidate = Kurento.getComplexType("IceCandidate")(_candidate);
+  const user: UserSession = UserRegistry.getById(sessionId);
 
-    if (user && user.webRtcEndpoint) {
-        user.addIceCandidate(candidate, name);
+  if (user && user.webRtcEndpoint) {
+    user.addIceCandidate(candidate, name);
+  } else {
+    if (!CachedData.candidatesQueue[user.id]) {
+      CachedData.candidatesQueue[user.id] = [];
     }
-    else {
-        if (!CachedData.candidatesQueue[user.id]) {
-            CachedData.candidatesQueue[user.id] = [];
-        }
-        CachedData.candidatesQueue[sessionId].push(candidate);
-    }
+    CachedData.candidatesQueue[sessionId].push(candidate);
+  }
 }
